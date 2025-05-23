826d9ecf3d598059139f31e0a84856984468e185
kazuhaaaaan <nouvaprasetyaa@gmail.com>
2025-05-23 21:59:00 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center : true,
+      padding: "16px",
+
+    },
+    extend: {
+      colors : {
+        "primary" : "#1d4ed8",
+        "secondary" : "#475569",
+        "dark" : "#020617",
+      }
+    },
+  },
+  plugins: [],
+}
+
