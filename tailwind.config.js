2b0d5467d50bf7e6e61282c33180e2cda25c9aa5
Nouva <nouvaprasetyaa@gmail.com>
2025-07-24 21:22:49 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
-      center : true,
-      padding: "16px",
-
+      center: true,
+      padding: '16px',
     },
     extend: {
-      colors : {
-        "primary" : "#1d4ed8",
-        "secondary" : "#475569",
-        "dark" : "#020617",
-      }
+      colors: {
+        primary: '#6366f1',
+        secondary: '#0ea5e9',
+        dark: '#334155',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
     },
   },
   plugins: [],
-}
-
+};
